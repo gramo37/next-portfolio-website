f70080189997f76d3494398ff7e63edc99dff940
LAPTOP29\Prasanna <prasanna.gramopadhye@optanium.com>
2023-11-07 18:57:00 +0530

    Added Skills, Work Experience, Projects and Education

--- server_utils/user.ts
+++ server_utils/user.ts
@@ -1,9 +1,68 @@
 import { prisma } from "@/db";
-import { TUser } from "@/types/user";
+import {
+  TEducation,
+  TProject,
+  TSkill,
+  TUser,
+  TWorkExperience,
+} from "@/types/user";
 
-export async function getUserInfo(): Promise<TUser> {
+type TUserInfo = TUser & {
+  skills: TSkill[];
+  workExperience: TWorkExperience[];
+  education: TEducation[];
+  project: TProject[];
+};
+
+export async function getUserInfo(): Promise<TUserInfo> {
   const user = await prisma.user.findUnique({
     where: { id: 1 },
+    select: {
+      name: true,
+      email: true,
+      description: true,
+      profession: true,
+      twitter_link: true,
+      linkedin_link: true,
+      github_link: true,
+      phone: true,
+      education: {
+        select: {
+          id: true,
+          degree_name: true,
+          university_name: true,
+          duration: true,
+          description: true,
+        },
+      },
+      skills: {
+        select: {
+          id: true,
+          skill_name: true,
+          proficiency: true,
+          maximum_proficiency: true,
+        },
+      },
+      project: {
+        select: {
+          id: true,
+          project_name: true,
+          description: true,
+          techStack: true,
+          background_img_url: true,
+          project_link: true,
+        },
+      },
+      workExperience: {
+        select: {
+          id: true,
+          company_name: true,
+          profession: true,
+          duration: true,
+          description: true,
+        },
+      },
+    },
   });
   if (!user) throw new Error("User not found!");
   return user;
@@ -16,4 +75,40 @@ export async function updateUserInfo(user: TUser) {
   });
   if (!updatedUser) throw new Error("User not updated! Try again..");
   return updatedUser;
+}
+
+export async function updateProjectInfo(project: TProject) {
+  const updatedProject = await prisma.project.update({
+    where: { id: 1 },
+    data: project,
+  });
+  if (!updatedProject) throw new Error("Project not updated! Try again..");
+  return updatedProject;
+}
+
+export async function updateSkillInfo(skill: TSkill) {
+  const updatedSkill = await prisma.skill.update({
+    where: { id: 1 },
+    data: skill,
+  });
+  if (!updatedSkill) throw new Error("Skill not updated! Try again..");
+  return updatedSkill;
+}
+
+export async function updateEducationInfo(education: TEducation) {
+  const updatedEducation = await prisma.education.update({
+    where: { id: 1 },
+    data: education,
+  });
+  if (!updatedEducation) throw new Error("Education not updated! Try again..");
+  return updatedEducation;
+}
+
+export async function updateWorkExperienceInfo(workExperience: TWorkExperience) {
+  const updatedWorkExperience = await prisma.workExperience.update({
+    where: { id: 1 },
+    data: workExperience,
+  });
+  if (!updatedWorkExperience) throw new Error("WorkExperience not updated! Try again..");
+  return updatedWorkExperience;
 }
--- types/user.ts
+++ types/user.ts
@@ -1,6 +1,43 @@
 export interface TUser {
-    id?: number;
-    email: string;
-    name: string;
-    description: string;
+  id?: number;
+  email: string;
+  name: string;
+  description: string[];
+  profession: string;
+  twitter_link: string;
+  linkedin_link: string;
+  github_link: string;
+  phone: string;
+}
+
+export interface TWorkExperience {
+  id?: number;
+  company_name: string;
+  profession: string;
+  duration: string;
+  description: string[];
+}
+
+export interface TEducation {
+  id?: number;
+  degree_name: string;
+  university_name: string;
+  duration: string;
+  description: string[];
+}
+
+export interface TSkill {
+  id?: number;
+  skill_name: string;
+  proficiency: number;
+  maximum_proficiency: number;
+}
+
+export interface TProject {
+  id?: number;
+  project_name: string;
+  description: string[];
+  techStack: string[];
+  background_img_url: string;
+  project_link: string;
 }
