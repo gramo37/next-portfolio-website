eb5335759566c69b44484decdf5d418c57c0e703
LAPTOP29\Prasanna <prasanna.gramopadhye@optanium.com>
2023-11-07 13:31:25 +0530

    Removed Caching

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import { getUserInfo } from "@/server_utils/user";
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export default async function Home() {
   const userInfo = await getUserInfo();
   if (!userInfo) return <h1>User not found</h1>;
